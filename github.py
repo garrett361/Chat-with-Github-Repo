ab46fae5b23877227392d8499a8bb4b2fefd3829
Garrett Goon <garrett.goon@hpe.com>
2023-04-29 16:30:07 -0400

    basics working

--- github.py
+++ github.py
@@ -1,14 +1,15 @@
 import os
 import subprocess
+
+import openai
+from dotenv import load_dotenv
 from langchain.document_loaders import TextLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import DeepLake
-from langchain.embeddings.openai import OpenAIEmbeddings
-from dotenv import load_dotenv
-import openai
 
 load_dotenv()
-openai.api_key = os.environ.get('OPENAI_API_KEY')
+openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 
 def clone_repository(repo_url, local_path):
@@ -20,11 +21,10 @@ def load_docs(root_dir):
     for dirpath, dirnames, filenames in os.walk(root_dir):
         for file in filenames:
             try:
-                loader = TextLoader(os.path.join(
-                    dirpath, file), encoding='utf-8')
+                loader = TextLoader(os.path.join(dirpath, file), encoding="utf-8")
                 docs.extend(loader.load_and_split())
             except Exception as e:
-                pass
+                print(f"Exception {e} raised but ignored; continuing.")
     return docs
 
 
@@ -38,15 +38,15 @@ def main(repo_url, root_dir, deep_lake_path):
     docs = load_docs(root_dir)
     texts = split_docs(docs)
     embeddings = OpenAIEmbeddings()
-    
+
     db = DeepLake(dataset_path=deep_lake_path, embedding_function=embeddings)
-    
+
     db.add_documents(texts)
 
 
 if __name__ == "__main__":
-    repo_url = os.environ.get('REPO_URL')
+    repo_url = os.environ.get("REPO_URL")
     root_dir = "./gumroad"
-    deep_lake_path = os.environ.get('DEEPLAKE_DATASET_PATH')
+    deep_lake_path = os.environ.get("DEEPLAKE_DATASET_PATH")
 
     main(repo_url, root_dir, deep_lake_path)
